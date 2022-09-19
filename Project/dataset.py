74f07911e2f9d2f774038918d465d3fb1ae68dd9
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-09-19 19:50:43 +1000

    change fileurl string format add self as parameter to loadCSVData add low_memory = false attribute to read_csv

--- Project/dataset.py
+++ Project/dataset.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 class Dataset:
     def __init__(self):
-        self.loadedData = self.loadCSVData("/data/penalty_data_set_2.csv")
+        self.loadedData = self.loadCSVData("data\penalty_data_set_2.csv")
 
-    def loadCSVData(CSVDataFile):
+    def loadCSVData(self, CSVDataFile):
         with open(CSVDataFile, 'r', encoding='utf-8') as csvData:
-           CSVDataFrame = pd.read_csv(csvData)
+           CSVDataFrame = pd.read_csv(csvData, low_memory=False)
            return CSVDataFrame
 
     def getData(self):
